53879dc5217e66193ee08bee32c8541691bc6ca9
Akash kumar <akashmahto1169@gmail.com>
2024-03-01 11:08:28 +0530

    accordion practice

--- src/component/accordion/AccordionRevise.jsx
+++ src/component/accordion/AccordionRevise.jsx
@@ -1,31 +1,47 @@
-import React, { useState } from 'react'
-import {data} from './data'
+import React, { useState } from "react";
+import { data } from "./data";
 
 const AccordionRevise = () => {
-    const [answer, setAnswer] = useState(null);
+  const [answer, setAnswer] = useState([]);
+  // const [multipleAnswer, setMultipleAnswer] = useState([])
 
-    const handleClick = (contentId)=>{
-        console.log(contentId)
-        setAnswer(contentId === answer ? null : contentId)
+  const handleClick = (contentId) => {
+    const multipleValue = [...answer];
+    const findindexofCurrentID = multipleValue.indexOf(contentId);
+    if (findindexofCurrentID === -1) {
+      multipleValue.push(contentId);
+    } else {
+      multipleValue.splice(findindexofCurrentID);
     }
+    setAnswer(multipleValue);
+    console.log(answer);
+  };
 
   return (
     <div>
       <div>
-            {data.map((item)=>(
-                <div key={item.id} className='gap-4'>
-                    <h1 className='m-4'>Question</h1>
-                    <div className='m-4'>
-                    <h1 onClick={()=>handleClick(item.id)}>{item.question}</h1>
-                    {answer === item.id ? (
-                    <p>Answer: {item.answer}</p>
-                    ) : null}
-                    </div>
-                </div>
-            ))}
+        {data.map((item) => (
+          <div key={item.id} className="gap-4">
+            <div className="m-4">
+              <h1 className="text-xl font-semibold">Question</h1>
+              <h1
+                onClick={() => handleClick(item.id)}
+                className="bg-blue-300 cursor-pointer"
+              >
+                {item.question}
+              </h1>
+              {answer.indexOf(item.id) !== -1 ? (
+                <p className="bg-green-400">
+                  <span className="text-xl font-semibold">Answer:</span>{" "}
+                  {item.answer}
+                </p>
+              ) : null}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AccordionRevise
+export default AccordionRevise;
