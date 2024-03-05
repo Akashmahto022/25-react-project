5ef2c21ab930b241e4698ba5b0b4e4a570ba863f
Akash kumar <akashmahto1169@gmail.com>
2024-03-05 10:09:43 +0530

    accordion practice

--- src/component/accordion/AccordionPractice2.jsx
+++ src/component/accordion/AccordionPractice2.jsx
@@ -1,37 +1,60 @@
-import React, { useState } from 'react'
-import {data} from "./data";
+import React, { useState } from "react";
+import { data } from "./data";
 
 const AccordionPractice2 = () => {
-    const [answer, setAnswer] = useState([])
+  const [answer, setAnswer] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multipleAnswer, setMultipleAnswer] = useState([]);
 
-    const handleClick =(contentID)=>{
-        const storeValue = [...answer]
-        const storeIndexOF = storeValue.indexOf(contentID)
-        if (storeIndexOF === -1) {
-            storeValue.push(contentID)
-        }
-        else{
-            storeValue.splice(storeIndexOF)
-        }
-        setAnswer(storeValue)
+  const handleClick = (contentID) => {
+    setAnswer(contentID === answer ? null : contentID);
+  };
+
+  const setMultiple = (contentID) => {
+    const storeID = [...multipleAnswer];
+    const multipleStoreIndexValue = storeID.indexOf(contentID);
+    if (multipleStoreIndexValue === -1) {
+      storeID.push(contentID);
+    } else {
+      storeID.splice(multipleStoreIndexValue, 1);
     }
+    setMultipleAnswer(storeID);
+  };
 
   return (
-    <div className='p-4'>
-      {
-        data.map((item)=>(
-            <div key={item.id}>
-                <h1 onClick={()=>{handleClick(item.id)}}>{item.question}</h1>
-                {answer.indexOf(item.id) !== -1 ? (
-                <p>{ item.answer}</p>
-                ) : null
-                 }
-            </div>
-        ))
-      }
+    <div className="p-4">
+      <button
+        onClick={() => {
+          setEnableMultiSelection(!enableMultiSelection)
+        }}
+        className="bg-blue-900 p-2 text-white rounded-md"
+      >
+        Set To Multiple
+      </button>
+      <div>Enable Multiple Selection: {enableMultiSelection === false ? "false" : "true"}</div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h1
+            onClick={
+              enableMultiSelection
+                ? () => {
+                    setMultiple(item.id);
+                  }
+                : () => {
+                    handleClick(item.id);
+                  }
+            }
+          >
+            {item.question}
+          </h1>
+          {enableMultiSelection ? 
+          ( multipleAnswer.indexOf(item.id) !== -1  && 
+            <div>{item.answer}</div>)
+           : (answer === item.id ? <p>{item.answer}</p> : null)}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-
-export default AccordionPractice2
+export default AccordionPractice2;
